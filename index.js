8d608a738380642fb48763bec01024d338286c89
Kerry <54252779+xxnuo@users.noreply.github.com>
2025-03-15 01:51:26 +0800

    Ref

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-import ADMIN_HTML from "./admin.html";
-import LOGIN_HTML from "./login.html";
-
 let KV_BINDING;
 
 // Cookie 相关函数
@@ -135,20 +132,6 @@ export default {
       return Response.redirect(`${url.origin}/login`, 302);
     }
 
-    if (path === 'wechat.svg') {
-      return new Response(
-        `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M15.85 8.14c.39 0 .77.03 1.14.08C16.31 5.25 13.19 3 9.44 3c-4.25 0-7.7 2.88-7.7 6.43c0 2.05 1.15 3.86 2.94 5.04L3.67 16.5l2.76-1.19c.59.21 1.21.38 1.87.47c-.09-.39-.14-.79-.14-1.21c-.01-3.54 3.44-6.43 7.69-6.43M12 5.89a.96.96 0 1 1 0 1.92a.96.96 0 0 1 0-1.92M6.87 7.82a.96.96 0 1 1 0-1.92a.96.96 0 0 1 0 1.92"/><path fill="currentColor" d="M22.26 14.57c0-2.84-2.87-5.14-6.41-5.14s-6.41 2.3-6.41 5.14s2.87 5.14 6.41 5.14c.58 0 1.14-.08 1.67-.2L20.98 21l-1.2-2.4c1.5-.94 2.48-2.38 2.48-4.03m-8.34-.32a.96.96 0 1 1 .96-.96c.01.53-.43.96-.96.96m3.85 0a.96.96 0 1 1 0-1.92a.96.96 0 0 1 0 1.92"/></svg>`
-        , {
-          headers: { 'Content-Type': 'image/svg+xml' }
-        });
-    }
-
-    if (path === 'login') {
-      return new Response(LOGIN_HTML, {
-        headers: { 'Content-Type': 'text/html' }
-      });
-    }
-
     // 管理后台路由处理
     if (path.startsWith('admin')) {
 
@@ -175,13 +158,6 @@ export default {
       }
 
       try {
-        // 管理页面
-        if (path === 'admin') {
-          return new Response(ADMIN_HTML, {
-            headers: { 'Content-Type': 'text/html' }
-          });
-        }
-
         // 映射列表
         if (path === 'admin/mappings') {
           const params = new URLSearchParams(url.search);
